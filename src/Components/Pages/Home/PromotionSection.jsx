b807c9332485ebad42d4e92d8cb3e5c0c53f5c43
aykutspohrdev <137683982+aykutspohrdev@users.noreply.github.com>
2023-07-09 13:37:19 +0200

    Fixed iOS outline-offset not working

--- src/Components/Pages/Home/PromotionSection.jsx
+++ src/Components/Pages/Home/PromotionSection.jsx
@@ -4,7 +4,6 @@ import promo2 from "../../../assets/Images/Home/Promotion-Images/ColdBrew_Coffee
 import promo3 from "../../../assets/Images/Home/Promotion-Images/Croissant_Promo_Image.webp";
 import promo4 from "../../../assets/Images/Home/Promotion-Images/Latte_Coffee_Promo_Image.webp";
 
-
 const PromotionSection = () => {
   return (
     <>
@@ -18,46 +17,55 @@ const PromotionSection = () => {
 
         <div className={classes.promotionContainer}>
           <div className={classes.promotionItem}>
-            <img
-              className={classes.promoImage}
-              src={promo1}
-              alt="Promotional art of a cake that The Coffee Coop serves"
-            />
-            <p1 className={classes.promoText}>
+            <div className={classes.imageContainer}>
+              <img
+                className={classes.promoImage}
+                src={promo1}
+                alt="Promotional art of a cake that The Coffee Coop serves"
+              />
+            </div>
+            <p className={classes.promoText}>
               We have our very own confectionery!
-            </p1>
+            </p>
           </div>
           <div className={classes.promotionItem}>
-            <img
-              className={classes.promoImage}
-              src={promo4}
-              alt="Promotional art of a Latte that The Coffee Coop serves"
-            />
-            <p1 className={classes.promoText}>
+            <div className={classes.imageContainer}>
+              <img
+                className={classes.promoImage}
+                src={promo4}
+                alt="Promotional art of a Latte that The Coffee Coop serves"
+              />
+            </div>
+            <p className={classes.promoText}>
               The best coffee in town, if we do say so ourselves!
-            </p1>
+            </p>
           </div>
           <div className={classes.promotionItem}>
-            <img
-              className={classes.promoImage}
-              src={promo2}
-              alt="Promotional art of a Cold Brew Coffee that The Coffee Coop serves"
-            />
-            <p1 className={classes.promoText}>
+            <div className={classes.imageContainer}>
+              <img
+                className={classes.promoImage}
+                src={promo2}
+                alt="Promotional art of a Cold Brew Coffee that The Coffee Coop serves"
+              />
+            </div>
+            <p className={classes.promoText}>
               Freshly brewed, refreshing Cold Brews! Yeah, we&apos;re in on the
               trend.
-            </p1>
+            </p>
           </div>
           <div className={classes.promotionItem}>
-            <img
-              className={classes.promoImage}
-              src={promo3}
-              alt="Promotional art of a Croissant that The Coffee Coop serves"
-            />
-            <p1 className={classes.promoText}>
+            <div className={classes.imageContainer}>
+              <img
+                className={classes.promoImage}
+                src={promo3}
+                alt="Promotional art of a Croissant that The Coffee Coop serves"
+              />
+            </div>
+
+            <p className={classes.promoText}>
               Want a little Paris for breakfast? We got you - with our
               croissants!
-            </p1>
+            </p>
           </div>
         </div>
       </div>
