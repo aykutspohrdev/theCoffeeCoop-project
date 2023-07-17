d1c312b5702f89c42fe1d0bc500435695b64d6f1
aykutspohrdev <137683982+aykutspohrdev@users.noreply.github.com>
2023-07-17 11:14:37 +0200

    Revert "Added _redirects file for refreshing on Netlify"
    This reverts commit ab7db0617456b938c1b0322cb4528d1a7e2b6db5.

--- src/Components/Pages/Courses/CourseOverview/CourseOverviewItem.jsx
+++ src/Components/Pages/Courses/CourseOverview/CourseOverviewItem.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { APIContext } from "src/Components/Context/APIContext";
 import classes from "./CourseOverviewItem.module.css";
+import image_0 from "src/assets/Images/Courses/CourseOverviewItem_Img_1.webp";
+import image_1 from "src/assets/Images/Courses/CourseOverviewItem_Img_2.webp";
+import image_2 from "src/assets/Images/Courses/CourseOverviewItem_Img_1.webp";
 
 const CourseOverviewItem = (props) => {
   const APIdata = useContext(APIContext);
@@ -15,6 +18,7 @@ const CourseOverviewItem = (props) => {
             className={classes.itemImage}
             src={
               `src/assets/Images/Courses/CourseOverviewItem_Img_${props.itemNum}.webp`
+              // APIdata.storeData.courseOverviewItems[`${props.itemNum}`].imageSrc
             }
           ></img>
         </div>
